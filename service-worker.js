72811479e112c4342d9fe98ce16795b0383a112d
abdallahmawed29-gif <abdallahmawed29@gmail.com>
2025-09-21 13:03:49 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,4 @@
-self.addEventListener('fetch', function(){ /* placeholder */ });
+const CACHE_NAME = 'abdullah-app-complete-v1';
+const urlsToCache = ['/', '/index.html'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
+self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(r => r || fetch(event.request))); });
